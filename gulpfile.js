85928e22e27075ef8989f5e2c7a926f6b4dcb72f
tugudush <jerome2kph@gmail.com>
2017-08-18 14:46:26 +0800

    gulp inject

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,25 @@
 var gulp = require('gulp');
 
 gulp.task('inject', function() {
+    
     var wiredep = require('wiredep').stream;
     var inject = require('gulp-inject');
+    
+    var injectSrc = gulp.src(['./css/*.css', './js/*.js']);
+    
+    var injectOptions = {
+        //ignorePath: '/public'
+    };
+    
     var options = {
         bowerJson: require('./bower.json'),
         directory: './bower_components'
         //ignorePath: '../../bower_components'
     };
+    
     return gulp.src('./*.html')
         .pipe(wiredep(options))
+        .pipe(inject(injectSrc, injectOptions))
         .pipe(gulp.dest('./'));
+    
 }); // end of gulp.task('inject', function()
